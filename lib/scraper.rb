b04d3f43bc2b33fef90a62eb16997dfadf33a6fc
Kevin Santos <kevinochieng548@gmail.com>
2018-10-01 13:33:10 +0300

    wrap site info in a nokogiri data object

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,6 @@ require 'Nokogiri'
 
 class Scraper
   doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
+  @parse_page ||= Nokogiri::HTML(doc)
 end
 
