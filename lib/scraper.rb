e491607e9f9a58e57822d77e6be9445424db8c27
Kevin Santos <kevinochieng548@gmail.com>
2018-10-01 15:18:03 +0300

    change the request url to https

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,10 @@
-require 'HTTParty'
-require 'Nokogiri'
+require 'httparty'
+require 'nokogiri'
 
 class Scraper
   attr_accessor :parse_page
   def initialize
-    doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
+    doc = HTTParty.get("https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
     @parse_page ||= Nokogiri::HTML(doc)
   end
 
