fd0f7b4aa7fb188658df920227bc9856dc4bf9a1
Kevin Santos <kevinochieng548@gmail.com>
2018-10-01 13:37:09 +0300

    place the parsed data into the initialize method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,9 @@ require 'HTTParty'
 require 'Nokogiri'
 
 class Scraper
-  doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
-  @parse_page ||= Nokogiri::HTML(doc)
+  def initialize
+    doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
+    @parse_page ||= Nokogiri::HTML(doc)
+  end
 end
 
