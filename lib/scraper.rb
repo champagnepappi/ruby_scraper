bf11008d2f461c60463251796697c5f4cdd66b4f
Kevin Santos <kevinochieng548@gmail.com>
2018-10-01 14:44:40 +0300

    loop and return names from the parse_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,5 +7,7 @@ class Scraper
     doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
     @parse_page ||= Nokogiri::HTML(doc)
   end
+
+  names = parse_page.css(".grid-item-info").css(".product-name").css("p").children.map {|name| name.text}.compact
 end
 
