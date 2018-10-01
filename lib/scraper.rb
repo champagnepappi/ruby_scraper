ad3dda226ad1a69732b0e33835b2ea79b70f0447
Kevin Santos <kevinochieng548@gmail.com>
2018-10-01 13:38:50 +0300

    add parse_page attr_accessor

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,7 @@ require 'HTTParty'
 require 'Nokogiri'
 
 class Scraper
+  attr_accessor :parse_page
   def initialize
     doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ipp=69")
     @parse_page ||= Nokogiri::HTML(doc)
